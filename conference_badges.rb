adb7d693a007ce4a5fc5dadf0b6632c21371c29b
favorable-vector-1743 <epzads@gmail.com>
2019-03-04 12:17:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker(name)
+  
+return "Hello, my name is #{name}."
+end 
+
+
+  
+ def batch_badge_creator(speakers)
+ new_array = []
+ speakers.each do |name|
+ new_array << badge_maker(name)
+#return array 
+end 
+return new_array
+end 
+
+def assign_rooms(rooms)
+
+#def printer(speak)
+#  speak.each do |name|
+# puts badge_maker(name)
+#end
+#end 
